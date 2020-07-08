ff359b2c9f2e089499d712b918abd9a5c51f0a12
andrew-bolt <andrew.bolt@coderus.com>
2020-07-08 12:00:16 +0100

    Initial commit

--- Quadratic Solver/ViewController.swift
+++ Quadratic Solver/ViewController.swift
@@ -10,11 +10,86 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var root1: UILabel!
+    @IBOutlet weak var root2: UILabel!
+    
+    @IBOutlet weak var a: UITextField!
+    @IBOutlet weak var b: UITextField!
+    @IBOutlet weak var c: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        root1.isHidden = true
+        root2.isHidden = true
     }
 
-
+    @IBAction func helpMe(_ sender: Any) {
+        let alertController =
+            UIAlertController(title: "About this method",
+        message: "A new alternate method proposed by Po-Shen Loh from Carnegie Mellon university. It calculates the average of the two roots from the sum of b. More detail here: https://www.poshenloh.com/quadraticdetail/", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Got it, thanks!", style: .default))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func solve(_ sender: Any) {
+    
+        print("I'm in! This is b.text: \(String(describing: b.text))")
+        
+        var A = Double(a.text ?? "1") ?? 1
+        var B = Double(b.text ?? "0") ?? 0
+        var C = Double(c.text ?? "0") ?? 0
+        
+        print("A = \(A)")
+        print("B = \(B)")
+        print("C = \(C)")
+        
+        if(A != 1) {
+            A = 1
+            B = B/A
+            C = C/A
+        }
+        
+        print("#########################")
+        print("A = \(A)")
+        print("B = \(B)")
+        print("C = \(C)")
+        
+        let average = -B / 2
+        
+        print("Average = \(average)")
+        
+        var u: Double {
+            return (pow(average, 2) - C).squareRoot()
+        }
+        
+        print("u = \(u)")
+        
+        let positiveRoot = average + u
+        let negativeRoot = average - u
+        
+        print("Positive root = \(positiveRoot)")
+        print("Negative root = \(negativeRoot)")
+        
+        if positiveRoot == Double.nan || negativeRoot == Double.nan {
+            root1.text = "No real roots"
+            root1.isHidden = false
+            root2.isHidden = true
+        }
+        
+        if positiveRoot != negativeRoot {
+            root1.text = "x₁: \(positiveRoot)"
+            root2.text = "x₂: \(negativeRoot)"
+            root1.isHidden = false
+            root2.isHidden = false
+        }
+        else if positiveRoot == negativeRoot {
+            root1.text = "x: \(positiveRoot)"
+            root1.isHidden = false
+            root2.isHidden = true
+        }
+        
+    }
+    
 }
 
--- Quadratic SolverUITests/Quadratic_SolverUITests.swift
+++ Quadratic SolverUITests/Quadratic_SolverUITests.swift
@@ -10,26 +10,18 @@ import XCTest
 
 class Quadratic_SolverUITests: XCTestCase {
 
+    var app: XCUIApplication?
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        app = XCUIApplication()
         continueAfterFailure = false
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        app?.textFields[AccessibilityIDs.aTextField].typeText("")
+        app?.textFields[AccessibilityIDs.bTextField].typeText("")
+        app?.textFields[AccessibilityIDs.cTextField].typeText("")
     }
 
     func testLaunchPerformance() throws {
@@ -39,5 +31,25 @@ class Quadratic_SolverUITests: XCTestCase {
                 XCUIApplication().launch()
             }
         }
+    }
+    
+    func testOneRoot() {
+        fillInForm(1, 2, 1)
+        XCTAssertTrue(((app?.otherElements[AccessibilityIDs.root1Label].exists) != nil))
+        XCTAssertFalse(((app?.otherElements[AccessibilityIDs.root2Label].exists) != nil))
+    }
+    
+    func testTwoRoots() {
+        fillInForm(1, 6, 5)
+    }
+    
+    func testNoRoots() {
+        fillInForm(1, 0, 1)
+    }
+    
+    func fillInForm(_ A: Double, _ B: Double, _ C: Double) {
+        app?.textFields[AccessibilityIDs.aTextField].typeText("\(A)")
+        app?.textFields[AccessibilityIDs.bTextField].typeText("\(B)")
+        app?.textFields[AccessibilityIDs.cTextField].typeText("\(C)")
     }
 }
